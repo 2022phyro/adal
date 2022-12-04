53cc18f40878c4b9a9c14f7657f086a988820602
2022phyro <phyrokelstein2@gmail.com>
2022-12-04 03:11:37 -0800

    No 10

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,27 +1,26 @@
 #include "lists.h"
-int rev(listint_t **head, int *array)
-{
-	int i = 0;
-
-	while ((*head))
-	{
-		array[i] = (*head)->n;
-		(*head) = (*head)->next;
-		i++;
-	}
-	return (i);
-}
+/**
+ * is_palindrome - checks if a linked lust is a palindrome
+ * @head: the head node
+ * Return: 1 or 0 dependinf if the node is a palindrome or not
+ */
 int is_palindrome(listint_t **head)
 {
 	int i, j, array[1024];
 
 	if ((*head) == NULL || (*head)->next == NULL)
 		return (1);
-	i = rev(head, array)
+	i = 0;
+	while ((*head))
+	{
+		array[i] = (*head)->n;
+		(*head) = (*head)->next;
+		i++;
+	}
 
 	for (j = 0; j < i; j++, i--)
 	{
-		if (array[i-1] != array[j])
+		if (array[i - 1] != array[j])
 		{
 			return (0);
 		}
