1078d37c3a02917c26456b8b9a94fe2111feceaf
2022phyro <phyrokelstein2@gmail.com>
2022-12-04 14:34:53 +0100

    Merge

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for ide in matrix:
         for mad in ide:
             if mad == ide[-1]:
-                print("{}".format(mad))
+                print("{}".format(mad), end="")
             else:
                 print("{}{}".format(mad, ' '), end="")
+        print()
