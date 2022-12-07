166ceb1064b71bab3015aa598c94a0a24ce954dc
2022phyro <phyrokelstein2@gmail.com>
2022-12-07 23:24:42 +0100

    Attempting Cpython

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,8 +2,6 @@
 def best_score(a_dictionary):
     try:
         a = (max(a_dictionary))
-        if a_dictionary.count(a) > 1:
-            return "None"
         return a
     except TypeError:
         return "None"
--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda y : map(lambda x : x ** 2, matrix ), matrix[::]))
-
-
-
-
+    return list(map(lambda y: list(map(lambda x: x ** 2, y[:])), matrix))
