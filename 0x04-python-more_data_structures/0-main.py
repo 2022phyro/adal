f3f7abd8de041d5fb3daccb6a64ed8eff8fc5476
2022phyro <phyrokelstein2@gmail.com>
2022-12-07 23:15:34 +0100

    Technical interview

--- 0x04-python-more_data_structures/0-main.py
+++ 0x04-python-more_data_structures/0-main.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-uniq_add = __import__('2-uniq_add').uniq_add
+square_matrix_map = \
+    __import__('101-square_matrix_map').square_matrix_map
 
-my_list = [1, 2, 3, 1, 4, 2, 5]
-result = uniq_add(my_list)
-print("Result: {:d}".format(result))
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+new_matrix = square_matrix_map(matrix)
+print(new_matrix)
+print(matrix)
--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     try:
-        return (max(a_dictionary))
-    except:
-        return None
+        a = (max(a_dictionary))
+        if a_dictionary.count(a) > 1:
+            return "None"
+        return a
+    except TypeError:
+        return "None"
