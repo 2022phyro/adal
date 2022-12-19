e3ef0d211303ddb7bb110e4612f0fd1ff090cb81
2022phyro <phyrokelstein2@gmail.com>
2022-12-19 13:48:07 +0100

    Handling errors in magic calculation.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+import sysi
+
+
 def safe_function(fct, *args):
     try:
         c = fct(*args)
         return c
     except:
-        print("Exception: {}".format(sys.exec_info()[1]), file=ys.stderr)
+        print("Exception: {}".format(sys.exec_info()[1]), file=sys.stderr)
         return (None)
