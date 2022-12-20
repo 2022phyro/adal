a052dcaa8f443b5a857ea07247b8c213253c250c
2022phyro <phyrokelstein2@gmail.com>
2022-12-20 14:45:39 +0100

    No 5 and 6

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -30,3 +30,13 @@ class Square:
         """This is the former area function.
         it gives the area of the square"""
         return (self.__size ** 2)
+
+    def my_print(self):
+        """This prints out the square area onto the screen"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
