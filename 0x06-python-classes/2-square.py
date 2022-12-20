faa9c2fd66beeda680cca00f3a1ff5e265730c8f
2022phyro <phyrokelstein2@gmail.com>
2022-12-20 12:55:18 +0100

    Add docstrings

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""This class initializes size but in this case, it checks
+     to see if the size is an appropriate value"""
+
+
 class Square:
+    """The class takes a value in a try and except block"""
     def __init__(self, size=0):
+        """This initializes the size attribue in a safe mode"""
         if type(size) != int:
             raise TypeError('size must be an integer')
         elif size < 0:
