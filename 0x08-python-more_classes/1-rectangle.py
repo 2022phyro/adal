735b3513fd2de1a45363dbeb044ae18ba7a6f768
2022phyro <phyrokelstein2@gmail.com>
2023-01-02 12:37:59 +0100

    No 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,13 +13,14 @@ class Rectangle:
             width (int, optional): the size. Defaults to 0.
             height (int, optional): the height. Defaults to 0.
         """
-        if not isinstance(width, int):
+        if type(width) != int:
             raise TypeError("width must be an integer")
-        if width < 0:
+        elif width < 0:
             raise ValueError("width must be >= 0")
-        self.width = width
-        if not isinstance(height, int):
+        elif type(height) != int:
             raise TypeError("height must be an integer")
-        if height < 0:
+        elif height < 0:
             raise ValueError("height must be >= 0")
-        self.height = height
+        else:
+            self.width = width
+            self.height = height
---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,16 @@
+import sys
+"""This module attempts to solve the famous N queens puzzle
+The N queens puzzle is the challenge of placing N non-attacking queens on
+an N x N chessboard
+"""
+if len(sys.argv) != 2:
+    print("Usage: nqueens N\n")
+    sys.exit(1)
+try:
+    c = int(sys.argv[1])
+except ValueError:
+    print("N must be a number\n")
+    sys.exit(1)
+if c < 4:
+    print("N must be at least 4\n")
+    sys.exit(1)
