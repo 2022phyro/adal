f0199791cd2e129889ca1b24ea49f2e5d6382d4c
2022phyro <phyrokelstein2@gmail.com>
2023-01-02 12:55:24 +0100

    Remove pycache

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -5,13 +5,13 @@ The N queens puzzle is the challenge of placing N non-attacking queens on
 an N x N chessboard
 """
 if len(sys.argv) != 2:
-    print("Usage: nqueens N\n")
+    sys.stderr.write("Usage: nqueens N\n")
     sys.exit(1)
 try:
     c = int(sys.argv[1])
 except ValueError:
-    print("N must be a number\n")
+    sys.stderr.write("N must be a number\n")
     sys.exit(1)
 if c < 4:
-    print("N must be at least 4\n")
+    sys.stderr.write("N must be at least 4\n")
     sys.exit(1)
