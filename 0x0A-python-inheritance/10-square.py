188e2b7409dd4ad33fc815d7394d3a6c64053b73
2022phyro <phyrokelstein2@gmail.com>
2023-01-11 19:51:55 +0100

    First commit: Create Readme and attemp no 1 to 100

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module contains a class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """This is a subclass of the Rectangle class
+    and a grandchild of the BaseGeometry class
+
+    Args:
+        Rectangle (class): The parent class
+    """
+    def __init__(self, size):
+        """Initializes the class
+
+        Args:
+            size (int): the dimensions for the square
+        """
+        super(Square, self).__init__(size, size)
---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""In this module, we will create a function
+to add attributes to classes, if we can
+"""
+
+
+def add_attribute(object, key, value):
+    pass
