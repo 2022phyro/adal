4c1909d7074bc54a89276a90ee05ebfb1dc18596
2022phyro <phyrokelstein2@gmail.com>
2023-02-15 00:19:43 +0100

    Attempted all mandatory tasks

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,4 @@
+-- This groups the scores aording to frequency
+SELECT score AS "score",
+COUNT(score) AS number FROM second_table
+GROUP BY score;
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,6 @@
 -- This creates second table onl yif second table doesn't exist
-CREATE TABLE second_table (id int, name varchar(256), score int);
+CREATE TABLE IF NOT EXISTS second_table 
+(id int, name varchar(256), score int);
 INSERT INTO second_table (id, name, score) VALUES
 	(1, 'John', 10),
 	(2, 'Alex', 3),
