beba6fa039fcf6075e9f3317aaac7aa08f6e8353
2022phyro <phyrokelstein2@gmail.com>
2023-02-15 01:42:50 +0100

    attempt no 17 again

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,11 +1,5 @@
 -- This converts the databse from utf8 to utf8mb4
-ALTER TABLE first_table
-CHANGE name name varchar(256)
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+USE `hbtn_0c_0`
 ALTER TABLE  first_table
 CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
-ALTER DATABASE hbtn_0c_0
-CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
