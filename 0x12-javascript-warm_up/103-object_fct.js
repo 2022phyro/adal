58b9574a580f871901fbf2a049b09039f38014c0
2022phyro <phyrokelstein2@gmail.com>
2023-03-13 18:08:34 +0100

    Semistandard check

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,11 +4,10 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr = function() {myObject.value++}
+myObject.incr = function () { myObject.value++; };
 myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
-
