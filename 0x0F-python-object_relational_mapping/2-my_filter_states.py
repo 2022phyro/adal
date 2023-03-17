43d583eb6034e6a12e2b122ebfaa3cecbb938b4c
2022phyro <phyrokelstein2@gmail.com>
2023-03-18 00:43:57 +0100

    Created a temporary readme and began attemtping from 0 to 5

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state = sys.argv[4]
+    db = MySQLdb.connect(host="localhost", user=username, password=password,
+            database=database, port=3306)
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM states WHERE name = "{}" ORDER BY id ASC'.format(state))
+    results = cursor.fetchall()
+    cursor.close()
+    db.close()
+    for result in results:
+        print(result)
