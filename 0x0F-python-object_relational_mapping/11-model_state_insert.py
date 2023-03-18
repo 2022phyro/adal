9592ec032dfabb160d92f8efeb5c74bc2d8bd3b5
2022phyro <phyrokelstein2@gmail.com>
2023-03-18 11:06:11 +0100

    Debugging session, removed erros from file 0 to 11/*/.py

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""This script lists all State objects that contain the letter
-a from the database hbtn_0e_6_usa"""
+"""This script adds the State object “Louisiana” to the database
+hbtn_0e_6_usa"""
 from model_state import Base, State
 import sys
-from sqlalchemy import create_engine, asc
+from sqlalchemy import create_engine, asc, desc
 from sqlalchemy.orm import sessionmaker
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
@@ -12,12 +12,16 @@ if __name__ == '__main__':
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
+    new_state = State()
+    new_state.name = "Louisiana"
+    session.add(new_state)
+    session.commit()
     states = session.query(State).filter(
-        State.name.contains("a")
-    ).order_by(asc(State.id)).all()
+        State.name == "Louisiana"
+    ).order_by(desc(State.id)).limit(1)
     session.close()
     if not states:
-        print("Nothing")
+        print("Not found")
     else:
         for state in states:
-            print(f"{state.id}: {state.name}")
+            print(f"{state.id}")
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,11 +4,15 @@ It uses SQLAlchemy to save to a
 certain database and simplfies the whole Query process
 by using ORM instead of SQL"""
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
+    """This class is actually a table in
+    our database. It is a table of states"""
+
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
-    name =  Column(String(128), nullable=False)
+    name = Column(String(128), nullable=False)
