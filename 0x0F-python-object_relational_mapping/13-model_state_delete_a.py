2ee4dcc90987060ad85245858b3da81f3313c79a
2022phyro <phyrokelstein2@gmail.com>
2023-03-18 23:14:29 +0100

    debugged no 2 and 13 /*/.py changed the strings to double quotes instead of single

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -12,6 +12,6 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
     for state in session.query(State):
-        if 'a' in state.name:
+        if "a" in state.name:
             session.delete(state)
     session.commit()
