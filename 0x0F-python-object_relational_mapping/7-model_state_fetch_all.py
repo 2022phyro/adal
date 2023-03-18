5f8b0d16ea1be60d1fcaf0f9ff4f230fe47720f5
2022phyro <phyrokelstein2@gmail.com>
2023-03-18 20:30:30 +0100

    merge

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """This script uses sqlalchemy to list all
 the states in a table"""
-from model_state import State
 import sys
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-if __name__ == '__main__':
+from sqlalchemy import create_engine
+from model_state import State
+if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
                             sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
