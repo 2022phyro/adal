70b47cbf38fadca1c4cf03d66feb0648adbd4af7
root <2022phyro>
2023-03-30 09:29:31 -0700

    No 0 and 1

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+#This takes a url and return the content lenght in bytes
+curl -sI "$1" | grep Content-Length | cut -d ' ' -f 2
---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+#This takes a url and return the content lenght in bytes
+curl -L "$1"
