2c0e70bfec42d47a965a451756f9084864474be9
2022phyro <phyrokelstein2@github.com>
2023-03-31 04:07:56 -0700

    Changed from 4 spaces to tabs

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,6 +4,6 @@ from urllib import request
 with request.urlopen('https://alx-intranet.hbtn.io/status') as res:
     html = res.read()
     print("Body response:")
-    print(f"    - type: {type(html)}")
-    print(f"    - content: {html}")
-    print(f"    - utf8 content: {html.decode(encoding='UTF-8')}")
+    print(f"\t- type: {type(html)}")
+    print(f"\t- content: {html}")
+    print(f"\t- utf8 content: {html.decode(encoding='UTF-8')}")
